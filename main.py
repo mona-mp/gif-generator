c79bc664ea594e87b3efa2dc9998cea25729e194
mona-mp <monamp1998@gmail.com>
2023-08-11 01:20:46 +0330

    add video to GIF Conversion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from video_to_gif.validation import is_valid_video
-from video_to_gif.video_tools import get_dimensions, get_fps, get_overlay_text
+from video_to_gif.video_tools import adjust_video, convert_to_gif, extract_video_segment, get_dimensions, get_fps, get_overlay_text, overlay_text
 
 # Logging configuration
 
@@ -12,6 +12,40 @@ file_path = input("Enter the path to the video file: ")
 # Validate the provided file path
 if is_valid_video(file_path):
     logging.info("Valid video!")
-    width, height = get_dimensions()
-    fps = get_fps()
-    text_content = get_overlay_text()
+    exit()
+
+# Get gif start time and duration from video
+start_time = float(input("Enter start time of the segment (in seconds): "))
+duration = float(input("Enter duration of the segment (in seconds): "))
+
+temp_video_path = "temp_video.mp4"
+extract_video_segment(file_path, start_time, duration, temp_video_path)
+
+# Get dimensions
+width, height = get_dimensions()
+
+# Get fps
+fps = get_fps()
+
+# Adjust video dimensions and FPS
+adjust_video(temp_video_path, width, height, fps, temp_video_path)
+
+#  Addoverlay text on the video
+overlay, text = get_overlay_text()
+
+if overlay:
+    # Get the position for the overlay from the user
+    valid_positions = ['top-left', 'top-right',
+                       'bottom-left', 'bottom-right', 'center']
+    position = ''
+    while position not in valid_positions:
+        position = input(
+            "Enter text position (top-left, top-right, bottom-left, bottom-right, center): ").strip().lower()
+        if position not in valid_positions:
+            logging.error(
+                f"Invalid position: {position}. Please select from the given choices.")
+    overlay_text(temp_video_path, text, position, temp_video_path)
+
+# Convert to GIF
+output_gif_path = "output.gif"
+convert_to_gif(temp_video_path, output_gif_path)
--- video_to_gif/video_tools.py
+++ video_to_gif/video_tools.py
@@ -1,5 +1,7 @@
 import logging
 
+import ffmpeg
+
 
 def get_dimensions():
     while True:  # Loop until a valid input is provided
@@ -39,3 +41,60 @@ def get_overlay_text():
             return False, None
         else:
             logging.error("Invalid choice. Please enter 'yes' or 'no'.")
+
+
+def extract_video_segment(input_path, start_time, duration, output_path):
+    try:
+        (
+            ffmpeg
+            .input(input_path, ss=start_time, t=duration)
+            .output(output_path, c='copy')
+            .run()
+        )
+    except ffmpeg.Error as e:
+        logging.error(f"Error while extracting segment: {e}")
+
+
+def adjust_video(input_path, width, height, fps, output_path):
+    try:
+        (
+            ffmpeg
+            .input(input_path)
+            .output(output_path, vf=f'scale={width}:{height}', r=fps)
+            .run()
+        )
+    except ffmpeg.Error as e:
+        logging.error(f"Error adjusting video: {e}")
+
+
+def overlay_text(input_path, text, position, output_path):
+    # Positions can be 'top-left', 'top-right', 'bottom-left', 'bottom-right', 'center'
+    position_mappings = {
+        'top-left': '(10,10)',
+        'top-right': '(main_w-text_w-10,10)',
+        'bottom-left': '(10,main_h-text_h-10)',
+        'bottom-right': '(main_w-text_w-10,main_h-text_h-10)',
+        'center': '(main_w/2-text_w/2,main_h/2-text_h/2)'
+    }
+
+    try:
+        (
+            ffmpeg
+            .input(input_path)
+            .output(output_path, vf=f'drawtext=text={text}:x={position_mappings[position]}:y={position_mappings[position]}:fontsize=24:fontcolor=white')
+            .run()
+        )
+    except ffmpeg.Error as e:
+        logging.error(f"Error overlaying text: {e}")
+
+
+def convert_to_gif(input_path, output_path):
+    try:
+        (
+            ffmpeg
+            .input(input_path)
+            .output(output_path, f='gif')
+            .run()
+        )
+    except ffmpeg.Error as e:
+        logging.error(f"Error converting to GIF: {e}")
